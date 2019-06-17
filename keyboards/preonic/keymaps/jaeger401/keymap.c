a8b0c98d5614767fa93761f76acb9905c039905b
Jon Gherardini <jaeger401@users.noreply.github.com>
2019-06-17 16:49:57 -0400

    Fix Shift/Enter key behavior

--- keyboards/preonic/keymaps/jaeger401/keymap.c
+++ keyboards/preonic/keymaps/jaeger401/keymap.c
@@ -35,7 +35,8 @@ enum preonic_keycodes {
   BACKLIT
 };
 
-#define SFTENT ACTION_MODS_TAP_KEY(MOD_RSFT, KC_ENT)
+// Borrowed from the Planck, but replaced by explicit definition of Shift+Enter keys in config.h
+// #define SFTENT ACTION_MODS_TAP_KEY(MOD_RSFT, KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -56,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFTENT,  \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,  \
   BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 

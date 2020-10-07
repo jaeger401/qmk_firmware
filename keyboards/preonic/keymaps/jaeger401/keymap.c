47cb35601b77267a8b7b6896216ae1e33fea88b3
Jon Gherardini <jaeger401@users.noreply.github.com>
2020-10-07 14:02:18 -0400

    First pass at copying cmd-tab to lower-tab

--- keyboards/preonic/keymaps/jaeger401/keymap.c
+++ keyboards/preonic/keymaps/jaeger401/keymap.c
@@ -36,7 +36,8 @@ enum preonic_keycodes {
   LOCKSCN,
   RMRUNTHS, // RubyMine run this spec/context
   RMRUN,    // RubyMine run spec/context
-  RMDEBUG   // RubyMine debug
+  RMDEBUG,  // RubyMine debug
+  CMDTAB    // cmd + tab
 };
 
 // Borrowed from the Planck, but replaced by explicit definition of Shift+Enter keys in config.h
@@ -122,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_preonic_grid(
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_DEL,
+  CMDTAB,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_DEL,
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_PIPE,
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_HOME, KC_END, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
@@ -238,6 +239,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case RMDEBUG:
           if (record->event.pressed) {
             SEND_STRING(SS_LCTL("d"));
+          }
+          return true;
+          break;
+        case CMDTAB:
+          if (record->event.pressed) {
+            SEND_STRING(SS_DOWN(X_LGUI));
+            SEND_STRING(SS_TAP(X_TAB));
+          } else {
+            SEND_STRING(SS_UP(X_LGUI));
           }
           return true;
           break;

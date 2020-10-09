73ba659400b8af82aede7aa1fa63976ae19132ab
Jon Gherardini <jaeger401@users.noreply.github.com>
2020-10-09 16:40:26 -0400

    Update movement keys in raise and adjust layers

--- keyboards/preonic/keymaps/jaeger401/keymap.c
+++ keyboards/preonic/keymaps/jaeger401/keymap.c
@@ -46,6 +46,16 @@ enum preonic_keycodes {
 // Borrowed from the Planck, but replaced by explicit definition of Shift+Enter keys in config.h
 // #define SFTENT ACTION_MODS_TAP_KEY(MOD_RSFT, KC_ENT)
 
+#define TABLEFT G(KC_LCBR)
+#define TABRGHT G(KC_RCBR)
+
+#define S_HOME S(KC_HOME)
+#define S_END  S(KC_END)
+#define S_UP   S(KC_UP)
+#define S_DOWN S(KC_DOWN)
+#define S_LEFT S(KC_LEFT)
+#define S_RGHT S(KC_RGHT)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
@@ -134,22 +144,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  | Back | Fwd  |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |      |      |      | Back | Fwd  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |TabLt |TabRt |   9  |   0  | Del  |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  | Home |  Up  | End  | PgUp |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  | Left | Down |  Up  |Right |  \   |
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  | Left | Down |Right | PgDn |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | Home | End  | PgUp | PgDn |      |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |TabLt |TabRt |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Prev | Vol- | Vol+ | Next |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    G(KC_LBRC),G(KC_RBRC),
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6, G(KC_LCBR),G(KC_RCBR),KC_9,   KC_0,    KC_DEL,
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSLS,
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_HOME, KC_END,  KC_PGUP, KC_PGDN, _______,
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    _______, _______, _______,G(KC_LBRC),G(KC_RBRC),
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______,
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_BSLS,
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  TABLEFT, TABRGHT, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
 ),
 
@@ -157,9 +167,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Reset|Debug |      |      |      |      |      |      |      |      |  Del |
+ * |      | Reset|Debug |      |      |      |      |      |ShHome|ShftUp|ShEnd |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |Aud on|AudOff|Qwerty|      |      |      |      |      |      |
+ * |      |      |      |Aud on|AudOff|Qwerty|      |      |ShLeft|ShDown|ShRght|      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |Voice-|Voice+|Mus on|MusOff|MidiOn|MidOff|      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -168,8 +178,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT_preonic_grid(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  _______, RESET,   DEBUG,   _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
-  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  QWERTY,  _______, _______, _______, _______, _______, _______,
+  _______, RESET,   DEBUG,   _______, _______, _______, _______, S_HOME,  S_UP,    S_END,   _______, _______,
+  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  QWERTY,  _______, S_LEFT,  S_DOWN,  S_RGHT,  _______, _______,
   _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BRID, KC_BRIU
 )
